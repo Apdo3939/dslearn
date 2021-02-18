9a058c5560e3ac44b3e67719501e9016a5fd1ca0
Apdo3939 <apdo3939@hotmail.com>
2021-02-18 17:32:13 -0300

    Course, Offer

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,17 @@ INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 3);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Html', 'D:\BootCamp03\img\html.png', 'D:\BootCamp03\img\htmlgray.png');
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Css', 'D:\BootCamp03\img\css.png', 'D:\BootCamp03\img\cssgray.png');
+INSERT INTO tb_course (name, img_uri, img_gray_uri) VALUES ('Java', 'D:\BootCamp03\img\java.png', 'D:\BootCamp03\img\javagray.png');
+
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('Basic', TIMESTAMP WITH TIME ZONE '2019-01-02T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-01-02T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('Premium', TIMESTAMP WITH TIME ZONE '2019-01-02T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-01-02T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('Full', TIMESTAMP WITH TIME ZONE '2019-01-02T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-01-02T03:00:00Z', 1);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('Basic', TIMESTAMP WITH TIME ZONE '2019-01-02T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-01-02T03:00:00Z', 2);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('Premium', TIMESTAMP WITH TIME ZONE '2019-01-02T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-01-02T03:00:00Z', 2);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('Full', TIMESTAMP WITH TIME ZONE '2019-01-02T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-01-02T03:00:00Z', 2);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('Basic', TIMESTAMP WITH TIME ZONE '2019-01-02T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-01-02T03:00:00Z', 3);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('Premium', TIMESTAMP WITH TIME ZONE '2019-01-02T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-01-02T03:00:00Z', 3);
+INSERT INTO tb_offer (edition, start_moment, end_moment, course_id) VALUES ('Full', TIMESTAMP WITH TIME ZONE '2019-01-02T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-01-02T03:00:00Z', 3);
